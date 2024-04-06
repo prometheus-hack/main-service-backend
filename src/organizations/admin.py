060cb5c55ddc8900570de32c0bd8de8c98e52f5c
through-your-tears <warstream55@gmail.com>
2024-04-06 07:10:50 +0300

    Фикс админки

--- src/organizations/admin.py
+++ src/organizations/admin.py
@@ -11,6 +11,12 @@ for model in models:
     admin.site.register(model)
 
 
+class OrganizationImageInline(admin.TabularInline):
+    model = OrganizationImage
+    readonly_fields = ['id']
+    extra = 1
+
+
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'phone', 'website']
