2c9ad37bbd2544118c00c54a40a6380f5f92e969
through-your-tears <warstream55@gmail.com>
2024-04-06 14:59:00 +0300

    Добавлены разные activities для пользователя

--- src/activities/urls.py
+++ src/activities/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PhotoListCreateAPIView, FavouriteOrganizationAPIView
+from .views import PhotoListCreateAPIView, FavouriteOrganizationAPIView, FavouriteOrganizationsListAPIView
 
 
 urlpatterns = [
     path('gallery/', PhotoListCreateAPIView.as_view()),
-    path('wishlist/<int:id>/favourite/', FavouriteOrganizationAPIView.as_view())
+    path('wishlist/<int:id>/favourite/', FavouriteOrganizationAPIView.as_view()),
+    path('wishlist/', FavouriteOrganizationsListAPIView.as_view())
 ]
